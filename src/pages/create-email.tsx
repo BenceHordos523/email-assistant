a94ade707989e4c2e80afa21845f65aaddd870f9
BenceHordos523 <Bence.Hordos523@gmail.com>
2023-04-23 21:22:28 +0200

    Working 1st version

--- src/pages/create-email.tsx
+++ src/pages/create-email.tsx
@@ -21,6 +21,15 @@ const removeBoschPart = (inputString: string) => {
   }
 };
 
+const removeHash = (inputString: string) => {
+  const startIndex = inputString.indexOf("####");
+  if (startIndex !== -1) {
+    return inputString.substring(startIndex + 4, inputString.length);
+  } else {
+    return inputString;
+  }
+};
+
 const EmailFeed = () => {
   const { data: emails } = api.emails.list.useQuery();
 
@@ -28,43 +37,51 @@ const EmailFeed = () => {
     <>
       <div className="flex w-full flex-col gap-8">
         {emails?.map((email) => {
+          email = {
+            ...email,
+            response: removeHash(email.response),
+          };
+
+          const splittedResponse = email.response.split("To-Do List:");
+
           return (
             <div
               className="flex w-full flex-row justify-evenly gap-3"
               key={email.id}
             >
-              <div className="w-50 flex h-[200px] gap-10 overflow-auto">
+              <div className="w-50 h-[200px] overflow-auto">
                 <div className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <p className="font-bold text-gray-700 dark:text-gray-400">
+                  <p className="whitespace-pre-line font-bold text-gray-700 dark:text-gray-400">
                     From: {email.from}
                   </p>
-                  <p className="mt-1 font-bold text-gray-700 dark:text-gray-400">
+                  <div className="mt-1 font-bold text-gray-700 dark:text-gray-400">
                     Subject: {email.subject}
-                  </p>
+                  </div>
                   <p
-                    className="mt-1 font-normal text-gray-700 dark:text-gray-400"
+                    className="mt-1 whitespace-pre-line font-normal text-gray-700 dark:text-gray-400"
                     style={{ fontFamily: "Courier New, monospace" }}
                   >
-                    Content: {email.content}
+                    Content: {"\n"}
+                    {email.content}
                   </p>
                 </div>
               </div>
               <div className="w-50 h-[200px] overflow-auto">
                 <div className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                   <p className="font-bold text-gray-700 dark:text-gray-400">
-                    Summary
+                    AI Response:
                   </p>
 
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
-                    {email.response}
+                  <div className="whitespace-pre-line font-normal text-gray-700 dark:text-gray-400">
+                    {splittedResponse[0]}
                   </div>
 
                   <p className="mt-2 font-bold text-gray-700 dark:text-gray-400">
                     To-Do:
                   </p>
 
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
-                    {email.response}
+                  <div className="whitespace-pre-line font-normal text-gray-700 dark:text-gray-400">
+                    {splittedResponse[1]}
                   </div>
                 </div>
               </div>
@@ -85,10 +102,13 @@ const CreateEmail: NextPage = () => {
   });*/
 
   const [emailContent, setEmailContent] = useState<emailContentType[]>([]);
-
-  const { mutateAsync } = api.emails.create.useMutation<emailContentType[]>({
+  const ctx = api.useContext();
+  const { mutateAsync, isLoading } = api.emails.create.useMutation<
+    emailContentType[]
+  >({
     onSuccess: () => {
-      console.log("Success!");
+      setEmailContent([]);
+      void ctx.emails.list.invalidate();
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
@@ -222,6 +242,29 @@ const CreateEmail: NextPage = () => {
             </form>
           </div>
         </div>
+        {isLoading && (
+          <div className="flex items-center justify-center">
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="mr-2 inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        )}
         <EmailFeed />
       </main>
     </>
--- src/server/api/routers/emails.ts
+++ src/server/api/routers/emails.ts
@@ -33,24 +33,27 @@ export const emailRouter = createTRPCRouter({
       )
     )
     .mutation(async ({ ctx, input }) => {
+      // first delete all emails from the user
+      await ctx.prisma.emailInformation.deleteMany({
+        where: {
+          userID: ctx.auth.userId,
+        },
+      });
+
       let emailContent = "";
       input.map((inputMail) => {
         emailContent += `#\nFrom: ${inputMail.from}\n`;
         emailContent += `Subject: ${inputMail.subject}\n`;
         emailContent += `Body: ${inputMail.content}\n#\n\n`;
-
-        emailContent += `#\nFrom: Florentin.Zsigovics@hu.bosch.com\n`;
-        emailContent += `Subject: Update QMM site\n`;
-        emailContent += `Body: Please update the QMM site until 14:00\n#\n`;
       });
 
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt:
-          "You are a great E-Mail Task handler. You will process multiple emails at once. Between each e-mail there will be a # sign, this will show you the start and the end of the email. Your duty is to createa a summary of each e-mail. Based on the summary and reasons create a To-Do list to each e-mail, which is list of actions to do. The summary and the action list must contain every important section of the e-mail, because these e-mails are from the workplace. You don't wanna miss any critical information. The to-do list should be critical and precise for each e-mail. Create the to-do list with reason and critical thinking. The summary should be short and precise for a workplace. Start each response with: From: the e-mail address, Subject: the subject of the e-mail and then the summary and the to-do list. After you process all the e-mails, the result should come back in a prio list. Urgent and critical cases should be first, then put smaller, meaningless e-mails later in the list. The priorization should be critical. If you are ready I will send you the e-mails. The To-Do List should be a list of actions items. The result you give me should be in the same format I give you, put #### between each e-mail response you make, but don't forget about the priorization. Process the emails: \n\n" +
+          "You are a great E-Mail Task handler. You will process multiple emails at once. Between each e-mail there will be a # sign, this will show you the start and the end of the email. Your duty is to createa a summary of each e-mail. Based on the summary and reasons create a To-Do list to each e-mail, which is list of actions to do. The summary and the action list must contain every important section of the e-mail, because these e-mails are from the workplace. You don't wanna miss any critical information. The to-do list should be critical and precise for each e-mail. Create the to-do list with reason and critical thinking. The summary should be short and precise for a workplace. Start each response with: From: the e-mail address, Subject: the subject of the e-mail and then the summary and the to-do list. After you process all the e-mails, the result should come back in a prio list. Urgent and critical cases should be first, then put smaller, meaningless e-mails later in the list. The priorization should be critical. If you are ready I will send you the e-mails. The To-Do List should be a list of actions items. The result you give me should be in the same format I give you, put #### between each e-mail response you make, but don't forget about the priorization. Process and prio the emails: \n\n" +
           emailContent,
         temperature: 0.2,
-        max_tokens: 1024,
+        max_tokens: 2048,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
@@ -61,22 +64,22 @@ export const emailRouter = createTRPCRouter({
       if (!responseText) {
         throw new TRPCError({
           code: "UNPROCESSABLE_CONTENT",
-          message: "No response from CahtGPT",
+          message: "No response from ChatGPT",
         });
       }
 
-      console.log(responseText);
+      const responseArray = responseText.split("\n\n");
 
-      /*input.map(async (inputMail) => {
+      input.map(async (inputMail, index) => {
         // openai calls here
 
         await ctx.prisma.emailInformation.create({
           data: {
             ...inputMail,
-            response: responseText,
+            response: responseArray[index] || "",
             userID: ctx.auth.userId,
           },
         });
-      });*/
+      });
     }),
 });
